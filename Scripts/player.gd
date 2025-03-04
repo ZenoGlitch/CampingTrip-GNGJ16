123644233fd60d8248a0fc43c8e3d28a4e065a33
Sebastian <sebastian.norderang.6229@student.uu.se>
2025-03-05 00:27:50 +0100

    camera movement with mouse

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,10 +1,17 @@
 extends Node3D
 
-@onready var camera = $Camera3D
+@onready var pivot = $CameraPivot
+@onready var camera = $CameraPivot/Camera3D
+
+@onready var sprite = $Sprite2D
 
 
 const deg90InRad = 1.5708
 
+const mouseSensitivity : float = 0.001
+
+enum direction {NORTH, WEST, SOUTH, EAST}
+var facingDir = direction.NORTH
 
 var target 
 # Called when the node enters the scene tree for the first time.
@@ -17,6 +24,18 @@ func _ready():
 func _process(delta):
 	pass
 
+func _unhandled_input(event: InputEvent):
+	if event is InputEventMouseButton:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	elif event.is_action_pressed("ui_cancel") :
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+	
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		if event is InputEventMouseMotion:
+			pivot.rotate_y(-event.relative.x * mouseSensitivity)
+			camera.rotate_x(-event.relative.y * mouseSensitivity)
+			pivot.rotation.y = clamp(pivot.rotation.y, deg_to_rad(-30), deg_to_rad(30))
+			camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-30), deg_to_rad(30))
 
 func _input(InputEvent):
 	if Input.is_action_just_pressed("TurnLeft"):
@@ -26,13 +45,46 @@ func _input(InputEvent):
 		RotateRight()
 		
 	if Input.is_action_just_pressed("TakePhoto"):
+		Screenshot()
 		print("Neeerp")
-		pass
+	
+	
 	
 	
 func RotateLeft():
 	self.rotate_y(deg90InRad)
+	match facingDir:
+		direction.NORTH:
+			facingDir = direction.WEST
+		direction.WEST:
+			facingDir = direction.SOUTH
+		direction.SOUTH:
+			facingDir = direction.EAST
+		direction.EAST:
+			facingDir = direction.NORTH
 	
 func RotateRight():
 	self.rotate_y(-deg90InRad)
+	match facingDir:
+		direction.NORTH:
+			facingDir = direction.EAST
+		direction.WEST:
+			facingDir = direction.NORTH
+		direction.SOUTH:
+			facingDir = direction.WEST
+		direction.EAST:
+			facingDir = direction.SOUTH
+
+func Screenshot():
+	var viewport = camera.get_viewport()
+	var texture = viewport.get_texture()
+	var img = texture.get_image()
+	img.save_png("user://activeImage.png")
+	LoadScreenshot()
+	
+func LoadScreenshot():
+	var image = Image.load_from_file("user://activeImage.png")
+	var texture = ImageTexture.create_from_image(image)
+	texture.set_size_override(Vector2i(1,1))
+	$Sprite2D.texture = texture
 	
