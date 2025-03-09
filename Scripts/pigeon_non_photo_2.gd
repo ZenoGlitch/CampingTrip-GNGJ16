3337769914b05c500c080cba7e66cec6b1878a99
Sebastian <sebastian.norderang.6229@student.uu.se>
2025-03-09 19:33:12 +0100

    pigeon npcs walking around

--- Scripts/pigeon_non_photo_2.gd
+++ Scripts/pigeon_non_photo_2.gd
@@ -1,10 +1,11 @@
 extends Sprite3D
 
-@onready var animation_player = $AnimationPlayer
+@onready var animation_player = $AnimationPlayer2
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	animation_player.play("move")
+	animation_player.play("move2")
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -12,10 +13,10 @@ func _process(_delta):
 	pass
 
 func _on_animation_player_animation_finished(anim_name):
-	if anim_name == "move":
+	if anim_name == "move2":
 		if self.is_flipped_h():
 			self.set_flip_h(false)
-			animation_player.play_backwards("move")
+			animation_player.play_backwards("move2")
 		else:
 			self.set_flip_h(true)
 			animation_player.play()
