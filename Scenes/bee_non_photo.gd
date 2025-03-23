d24deddce9ca4d57e6b195ee91081a62789910c7
Sebastian <sebastian.norderang.6229@student.uu.se>
2025-03-23 15:48:15 +0100

    animated one bee

---
+++ Scenes/bee_non_photo.gd
@@ -0,0 +1,13 @@
+extends Sprite3D
+
+@onready var animation_player = $"../BeeAnimationPlayer"
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	animation_player.play("Bee1_move")
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
